c11bf750f84918e376e369665a82ef60427d9acb
AlexPranschke <alexpranschke@gmail.com>
2024-06-03 11:10:32 +0200

    deploy

--- src/styles/styles.tsx
+++ src/styles/styles.tsx
@@ -3,7 +3,8 @@ const Tail = {
     ics: "rounded-md mb-6 h-12",
     ics2: "rounded-md mb-6 h-24",
     bcs: "rounded-md border hover:bg-gray-300 active:bg-gray-400",
-    hcs: "text-3xl m-6"
+    hcs: "text-3xl m-6",
+    h2cs: "text-2xl m-6"
 };
 
 export default Tail;
